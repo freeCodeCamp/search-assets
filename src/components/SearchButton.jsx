6674f70a449ae44df7743bd110a8ad99666c9773
Stuart Taylor <staylor1012@gmail.com>
2017-06-18 19:56:10 +0100

    Pressing enter key submits search query

--- src/components/SearchButton.jsx
+++ src/components/SearchButton.jsx
@@ -1,44 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-import socket from '../websocket';
-
-const propTypes = {
-  searchTerm: PropTypes.string,
-  updateResults: PropTypes.func.isRequired
-};
-
-socket.on('connection', () => {
-  console.info('connected to elasticsearch!');
-});
-
-export default class SearchButton extends React.PureComponent {
-  constructor() {
-    super();
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    const {
-      searchTerm,
-      updateResults
-    } = this.props;
-    socket.emit(
-      'search-for-this',
-      JSON.stringify({ text: searchTerm }),
-      (response) => {
-        updateResults(response);
-      }
+export default function SearchButton() {
+  return (
+    <button type='submit'>Search</button>
     );
-  }
-
-  render() {
-    return (
-      <button onClick={ this.handleClick }>Search</button>
-      );
-  }
 }
 
 SearchButton.displayName = 'SearchButton';
-SearchButton.propTypes = propTypes;
