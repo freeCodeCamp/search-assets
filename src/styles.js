8893c56c22fdf203811e12f7d7fbabf6a96f8498
Stuart Taylor <staylor1012@gmail.com>
2017-06-22 05:49:06 +0100

    Results should be play nicely with surrounding elements

--- src/styles.js
+++ src/styles.js
@@ -4,17 +4,28 @@ export const input = {
 
 export const wrapper = {
   display: 'flex',
-  flexDirection: 'row'
+  flexDirection: 'row',
+  marginBottom: '0'
 };
 
 export const resultUl = {
   marginTop: '0',
+  marginLeft: '0',
   borderTop: 'none',
-  boxShadow: ' -1px 3px 19px 1px rgba(0,100,0,0.25)',
-  listStyle: 'none'
+  boxShadow: ' -1px 3px 19px 1px rgba(51, 51, 51, 0.25)',
+  listStyle: 'none',
+  position: 'absolute',
+  color: '#333',
+  background: '#fff',
+  width: '100%'
+};
+
+export const resultLi = {
+  marginTop: '5px',
+  marginLeft: '5px',
+  marginRIght: '5px'
 };
 
 export const appWrap = {
-  display: 'flex',
-  flexDirection: 'column'
+  position: 'relative'
 };
