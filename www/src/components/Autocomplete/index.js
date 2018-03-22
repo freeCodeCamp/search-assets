dd59eaf2ddefdcf4af72eff896880cd69f866fce
Stuart Taylor <staylor1012@gmail.com>
2018-03-22 22:02:44 +0000

    Use SearchHits in pages/index.js

--- www/src/components/Autocomplete/index.js
+++ www/src/components/Autocomplete/index.js
@@ -1,89 +1,68 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connectAutoComplete } from 'react-instantsearch/connectors';
-import { Highlight } from 'react-instantsearch/dom';
 
-import './autocomplete.css';
-
-const blockTitleMap = {
-  challenges: 'Lessons',
-  guides: 'Guide',
-  youtube: 'YouTube'
+const propTypes = {
+  currentRefinement: PropTypes.string,
+  refine: PropTypes.func.isRequired
 };
 
-const Autocomplete = ({ hits, currentRefinement, refine }) => {
-  const isHitsEmpty = hits.every(index => !index.hits.length);
+const submit = (
+  <svg
+    className="ais-SearchBox-submitIcon"
+    height="10"
+    viewBox="0 0 40 40"
+    width="10"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d={
+        'M26.804 29.01c-2.832 2.34-6.465 3.746-10.426 3.746C7.333 32.756 0 2' +
+        '5.424 0 16.378 0 7.333 7.333 0 16.378 0c9.046 0 16.378 7.333 16.378' +
+        ' 16.378 0 3.96-1.406 7.594-3.746 10.426l10.534 10.534c.607.607.61 1' +
+        '.59-.004 2.202-.61.61-1.597.61-2.202.004L26.804 29.01zm-10.426.627c' +
+        '7.323 0 13.26-5.936 13.26-13.26 0-7.32-5.937-13.257-13.26-13.257C9.' +
+        '056 3.12 3.12 9.056 3.12 16.378c0 7.323 5.936 13.26 13.258 13.26z'
+      }
+    />
+  </svg>
+);
+
+function Autocomplete(props) {
+  const { currentRefinement, refine } = props;
 
   return (
     <div className="ais-SearchBox">
-      <form className="ais-SearchBox-form" noValidate>
+      <form
+        className="ais-SearchBox-form"
+        noValidate={true}
+        ref={ref => {this.__formRef__ = ref;}}
+      >
         <input
-          className="ais-SearchBox-input"
+          autoCapitalize="off"
           autoComplete="off"
           autoCorrect="off"
-          autoCapitalize="off"
+          className="ais-SearchBox-input"
+          maxLength="512"
+          onChange={event => refine(event.currentTarget.value)}
           placeholder="Search for products"
           spellCheck="false"
-          maxLength="512"
           type="search"
           value={currentRefinement}
-          onChange={event => refine(event.currentTarget.value)}
         />
         <button
           className="ais-SearchBox-submit"
-          type="submit"
           title="Submit the search query."
+          type="submit"
         >
-          <svg
-            className="ais-SearchBox-submitIcon"
-            xmlns="http://www.w3.org/2000/svg"
-            width="10"
-            height="10"
-            viewBox="0 0 40 40"
-          >
-            <path d="M26.804 29.01c-2.832 2.34-6.465 3.746-10.426 3.746C7.333 32.756 0 25.424 0 16.378 0 7.333 7.333 0 16.378 0c9.046 0 16.378 7.333 16.378 16.378 0 3.96-1.406 7.594-3.746 10.426l10.534 10.534c.607.607.61 1.59-.004 2.202-.61.61-1.597.61-2.202.004L26.804 29.01zm-10.426.627c7.323 0 13.26-5.936 13.26-13.26 0-7.32-5.937-13.257-13.26-13.257C9.056 3.12 3.12 9.056 3.12 16.378c0 7.323 5.936 13.26 13.258 13.26z" />
-          </svg>
+          {submit}
         </button>
       </form>
-
-      {currentRefinement &&
-        !isHitsEmpty && (
-          <div className="ais-Hits">
-            <ul className="ais-Hits-list">
-              {hits.map(({ hits: results, index }) =>
-                results.map(result => (
-                  <li
-                    className="ais-Hits-item dataset-node"
-                    data-fcc-content-index={index}
-                    data-fcc-content-url={
-                      'videoId' in result ? result.videoId : result.url
-                    }
-                    key={result.objectID}
-                    onClick={event => console.log(event.target)}
-                  >
-                    <p>
-                      <strong>{blockTitleMap[index]}</strong>
-                      &nbsp;
-                      <Highlight attribute="title" hit={result} />
-                    </p>
-                  </li>
-                ))
-              )}
-            </ul>
-          </div>
-        )}
-
-      {currentRefinement &&
-        isHitsEmpty && (
-          <div className="ais-Hits">
-            <ul className="ais-Hits-list">
-              <li className="ais-Hits-item">
-                We could not find anything relating {currentRefinement}
-              </li>
-            </ul>
-          </div>
-        )}
     </div>
   );
-};
+}
+
+Autocomplete.displayName = 'AutoComplete';
+Autocomplete.propTypes = propTypes;
 
 export default connectAutoComplete(Autocomplete);
