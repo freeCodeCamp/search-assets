ed492fe7ce6d5c6f11e558cbf1ba0796c84991ba
Stuart Taylor <staylor1012@gmail.com>
2018-03-22 10:23:07 +0000

    Pull query from location and search on mount

--- www/src/components/Header/index.js
+++ www/src/components/Header/index.js
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { SearchBox } from 'react-instantsearch/dom';
+import qs from 'query-string';
 
 import './header.css';
 
-const Header = () => (
-  <header>
-    <nav>
-      <div className="nav-logo">
-        <img
-          alt="freeCodeCamp logo. Learn to code and help non-profits"
-          src="https://s3.amazonaws.com/freecodecamp/freecodecamp_logo.svg"
-        />
-      </div>
-      <SearchBox
-        translations={{
-          placeholder: 'Search 8,000+ lessons, articles, and videos'
-        }}
-      />
-    </nav>
-  </header>
-);
+class Header extends PureComponent {
+  componentDidMount() {
+    const { props: searchBoxProps, refine } = this._SearchBox_;
+    const { defaultRefinement } = searchBoxProps;
+    return defaultRefinement ? refine(defaultRefinement) : null;
+  }
+
+  getQueryFromLocation() {
+    if (typeof window === 'undefined') {
+      return '';
+    }
+    const { q: query } = qs.parse(window.location.search);
+    return query ? query : '';
+  }
+
+  render() {
+    return (
+      <header>
+        <nav>
+          <div className="nav-logo">
+            <img
+              alt="freeCodeCamp logo. Learn to code and help non-profits"
+              src="https://s3.amazonaws.com/freecodecamp/freecodecamp_logo.svg"
+            />
+          </div>
+          <SearchBox
+            defaultRefinement={this.getQueryFromLocation()}
+            ref={ref => {
+              this._SearchBox_ = ref;
+            }}
+            translations={{
+              placeholder: 'Search 8,000+ lessons, articles, and videos'
+            }}
+          />
+        </nav>
+      </header>
+    );
+  }
+}
 
 export default Header;
